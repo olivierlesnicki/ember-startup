8b87b21c2feb7979d5c6a0b80f59d62aae227f3e
Olivier Lesnicki <olivier.lesnicki@gmail.com>
2016-05-09 19:20:01 +0100

    Added some stuff

--- blueprints/ember-startup/index.js
+++ blueprints/ember-startup/index.js
@@ -1,6 +1,14 @@
 /*jshint node:true*/
+'use strict';
+
 module.exports = {
   description: '',
+
+  normalizeEntityName() {
+    // this prevents an error when the entityName is
+    // not specified (since that doesn't actually matter
+    // to us
+  },
 
   // locals: function(options) {
   //   // Return custom template variables here.
@@ -9,12 +17,17 @@ module.exports = {
   //   };
   // }
 
-  afterInstall: function(options) {
-    return this.addAddonsToProject([{
-      name: 'ember-cli-less',
-      target: '1.5.3'
-    }]).then(function () {
-      return addBowerPackagesToProject([{
+  afterInstall(options) {
+    return this.addAddonsToProject({
+      packages: [{
+        name: 'ember-cli-less',
+        target: '1.5.3'
+      }, {
+        name: 'ember-component-css',
+        target: '0.2.0-beta-4'
+      }]
+    }).then(() => {
+      this.addBowerPackagesToProject([{
         name: 'normalize-css',
         target: 'normalize.css#^4.1.1'
       }]);
--- index.js
+++ index.js
@@ -5,9 +5,14 @@ var path = require('path');
 
 module.exports = {
   name: 'ember-startup',
+
   included: function(app) {
     this._super.included(app);
+    app.import('/bower_components/normalize-css/normalize.css');
+  },
 
-    app.import(path.join(app.bowerDirectory), 'normalize-css/normalize.css');
+  isDevelopingAddon: function() {
+    return true;
   }
+
 };
